862c26ea982dd489b4c5a1d67c23aaf09476c2ef
Kaitomy <99449281+Kaitomy@users.noreply.github.com>
2022-11-17 16:05:53 +0300

    Pr1

--- src/main/java/com/example/Proj/controller/MainController.java
+++ src/main/java/com/example/Proj/controller/MainController.java
@@ -3,15 +3,22 @@ package com.example.Proj.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class MainController {
     @GetMapping("/home")
-    public String index(@RequestParam(name = "name", required = false, defaultValue = "Привет") String id, Model model) {
-        model.addAttribute("temp", id);
+    public String index(@RequestParam(name = "id", required = false, defaultValue = "Привет") String text, Model model) {
+        model.addAttribute("temp", text);
 
-        System.out.println(id);
+        System.out.println(text);
+        return "home";
+    }
+    @PostMapping("/home")
+    public String index2(@RequestParam(name = "id", required = false, defaultValue = "Привет") String text, Model model) {
+        model.addAttribute("temp", text);
+        System.out.println(text);
         return "home";
     }
 
