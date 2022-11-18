00f90e6641fcdf77eb8c5afd8218a1db347ce332
Kaitomy <99449281+Kaitomy@users.noreply.github.com>
2022-11-18 10:45:22 +0300

    Pr1-Calculator

--- src/main/java/com/example/Proj/controller/MainController.java
+++ src/main/java/com/example/Proj/controller/MainController.java
@@ -1,5 +1,6 @@
 package com.example.Proj.controller;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -8,22 +9,56 @@ import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class MainController {
-    @GetMapping("/home")
-    public String index(@RequestParam(name = "id", required = false, defaultValue = "Привет") String text, Model model) {
-        model.addAttribute("temp", text);
+    float itog1;
 
-        System.out.println(text);
-        return "home";
+    @GetMapping("/home")
+    public String index(@RequestParam(required = false) float number, float number2, String action, Model model) {
+
+
+            if (action.contains("P")) {
+                itog1 = number + number2;
+            }
+            if (action.contains("M")) {
+                itog1 = number - number2;
+            }
+            if (action.contains("Y")) {
+                itog1 = number * number2;
+            }
+            if (action.contains("D")) {
+                itog1 = number / number2;
+            }
+            model.addAttribute("number", number);
+            model.addAttribute("number2", number2);
+            model.addAttribute("itog1", itog1);
+            return "home";
+
+
     }
-    @PostMapping("/home")
-    public String index2(@RequestParam(name = "id", required = false, defaultValue = "Привет") String text, Model model) {
-        model.addAttribute("temp", text);
-        System.out.println(text);
-        return "home";
+
+    @PostMapping("/about")
+    public String index2(@RequestParam(required = false) float number, float number2, String action, Model model) {
+        if (action.contains("P")) {
+            itog1 = number + number2;
+        }
+        if (action.contains("M")) {
+            itog1 = number - number2;
+        }
+        if (action.contains("Y")) {
+            itog1 = number * number2;
+        }
+        if (action.contains("D")) {
+            itog1 = number / number2;
+        }
+        model.addAttribute("number", number);
+        model.addAttribute("number2", number2);
+        model.addAttribute("itog1", itog1);
+        return "about";
     }
 
     @GetMapping("/about")
     public String about() {
         return "about";
     }
+
+
 }
